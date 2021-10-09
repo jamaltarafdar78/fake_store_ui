03a7821dc36684d5ed5a626582d071a824462fdf
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2021-10-09 16:23:10 +0100

    update App test to ensure it calls the dispatcher with the correct type for stop and restart loading

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { AppStatusTypes } from './redux/reducers/app-status';
 
 const mockDispatcher = jest.fn();
 
-test('renders learn react link', () => {
-  render(<App dispatcher={mockDispatcher} state={{ appStatus: 'Loading' }} />);
-  const stopLoading = screen.getByText(/Stop Loading/i);
-  expect(stopLoading).toBeInTheDocument();
+describe('<App />', () => {
+  beforeEach(() => {
+    render(
+      <App dispatcher={mockDispatcher} state={{ appStatus: 'Loading' }} />
+    );
+  });
 
-  const loading = screen.getByText(/Restart Loading/i);
-  expect(loading).toBeInTheDocument();
+  afterEach(() => {
+    mockDispatcher.mockReset();
+  });
+
+  test('App dispatches Loading type when mounted', () => {
+    expect(mockDispatcher).toHaveBeenCalledWith(AppStatusTypes.LOADING);
+  });
+
+  test('Stop loading is available and connected to dispatcher correctly', () => {
+    const stopLoading = screen.getByText(/Stop Loading/i);
+    expect(stopLoading).toBeInTheDocument();
+
+    userEvent.click(stopLoading);
+
+    expect(mockDispatcher).toHaveBeenLastCalledWith(AppStatusTypes.DATA_LOADED);
+  });
+
+  test('Restart loading is available and connected to dispatcher correctly', () => {
+    const restartLoading = screen.getByText(/Restart Loading/i);
+    expect(restartLoading).toBeInTheDocument();
+    userEvent.click(restartLoading);
+
+    expect(mockDispatcher).toHaveBeenLastCalledWith(AppStatusTypes.LOADING);
+  });
 });
