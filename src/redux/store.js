e878269d5542908e7818fe47cd11ee0a63b26d55
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2021-10-09 16:35:05 +0100

    Use redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)(createStore)(rootReducer);
 
 export default store;
